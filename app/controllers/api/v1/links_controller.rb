03ac1d85c60ee44415049d0594ad7a5ecca7cd21
Brandon Rojas <branrojs@gmail.com>
2019-06-24 23:25:07 -0600

    Commit[2]: Adding Link controller and routes

---
+++ app/controllers/api/v1/links_controller.rb
@@ -0,0 +1,47 @@
+module Api
+  module V1
+    class LinksController < ApplicationController
+      def index
+        links = Link.order('created_at DESC')
+        render json: {
+                        status: 'SUCCESS',
+                        message: 'Loaded Links',
+                        data: links
+                     }, status: :ok
+      end
+
+      def show
+        link = Link.find(params[:id])
+        render json: {
+          status: 'SUCCESS',
+          message: 'Loaded Link',
+          data: link
+       }, status: :ok
+      end
+
+      def create
+        link = Link.new(link_params)
+
+        if link.save
+          render json: {
+            status: 'SUCCESS',
+            message: 'Link saved',
+            data: link
+                       }, status: :ok
+        else
+          render json: {
+            status: 'ERROR',
+            message: 'Link not saved',
+            data: link
+                       }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def link_params
+        params.permit(:title, :URL, :SURL, :visits)
+      end
+    end
+  end
+end
