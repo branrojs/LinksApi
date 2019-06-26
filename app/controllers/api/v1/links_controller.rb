bc3853359281e4f824a7189906529450477c2f04
Brandon <brandon.rojas@alkaidconsulting.com>
2019-06-26 00:33:51 -0600

    Commit[7]: * Changes on the links controller * Changes on the links model

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -2,46 +2,53 @@ module Api
   module V1
     class LinksController < ApplicationController
       def index
-        links = Link.order('visites DESC')
+        links = Link.order('visites DESC').limit(100)
         render json: {
-                        status: 'SUCCESS',
-                        message: 'Loaded Links',
-                        data: links
-                     }, status: :ok
+          status: 'SUCCESS',
+          message: 'Loaded Links',
+          data: links
+        }, status: :ok
       end
 
       def show
-        link = Link.find(params[:id])
-        render json: {
-          status: 'SUCCESS',
-          message: 'Loaded Link',
-          data: link
-       }, status: :ok
+        @link = Link.find_by(short_url: params[:id])
+        if @link.present?
+          aument = @link.visites
+          Link.update(@link.id, visites: (aument + 1))
+          redirect_to @link.url, status: 301
+        else
+          render json: {
+            status: 'ERROR', message: 'Link does not exist', data: link
+          }, status: :unprocessable_entity
+        end
       end
 
       def create
-        link = Link.new(link_params)
-
+        link = Link.new(link_params_to_create)
         if link.save
+
+          LinkJob.perform_later link.url
+
           render json: {
-            status: 'SUCCESS',
-            message: 'Link saved',
-            data: link
-                       }, status: :ok
+            status: 'SUCCESS', message: 'Link saved', data: link.url
+          }, status: :ok
         else
           render json: {
-            status: 'ERROR',
-            message: 'Link not saved',
-            data: link
-                       }, status: :unprocessable_entity
+            status: 'ERROR', message: 'Link not saved', data: link
+          }, status: :unprocessable_entity
         end
       end
 
       private
 
-      def link_params
-        params.permit(:title, :URL, :SURL, :visits)
+      def link_params_to_create
+        params.permit(:url)
       end
+
+      def link_params_to_update
+        params.permit(:title, :url, :short_url, :visites)
+      end
+
     end
   end
 end
